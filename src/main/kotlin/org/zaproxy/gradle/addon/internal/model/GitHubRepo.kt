1a1491548d88f12b56e49cbfdd5f644dde5f6ff5
thc202 <thc202@gmail.com>
2024-06-17 08:32:57 +0100

    Update Gradle, add Java 22 to CI
    Update Gradle Wrapper to 8.8 and ZAP API client to latest version.
    Update GitHub Actions in workflows and add Java 22 to CI.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- src/main/kotlin/org/zaproxy/gradle/addon/internal/model/GitHubRepo.kt
+++ src/main/kotlin/org/zaproxy/gradle/addon/internal/model/GitHubRepo.kt
@@ -12,7 +12,10 @@ data class GitHubRepo(val owner: String, val name: String, val dir: File? = null
     override fun toString() = "$owner/$name"
 }
 
-fun split(value: String?, pos: Int): String {
+fun split(
+    value: String?,
+    pos: Int,
+): String {
     val values = value?.split('/')
     return if (values?.size == 2) values[pos] else ""
 }
